1bf1bcf2ed4e1db7ee91cb0028f40a938eca42e8
Nils Werner <nils.jp.werner@gmail.com>
2015-11-11 01:13:10 -0600

    more stuff added

--- Marketplace/urls.py
+++ Marketplace/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    
+    # Static pages
+    url(r'^$', TemplateView.as_view(template_name="Marketplace/home.html")),
+
     # Profile tools
     #url(r'^accounts/', include('accounts.urls', namespace="accounts")),
     url(r'^accounts/', include('allauth.urls')),
