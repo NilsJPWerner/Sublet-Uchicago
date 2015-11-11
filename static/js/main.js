b80e490255f8a6bac860b1da7239b309873faef9
Nils Werner <nils.jp.werner@gmail.com>
2015-11-11 00:29:10 -0600

    built a menu

--- static/js/main.js
+++ static/js/main.js
@@ -1,11 +1,23 @@
-$('.example .menu .browse')
+$('#categories')
   .popup({
     inline   : true,
     hoverable: true,
     position : 'bottom left',
     delay: {
-      show: 300,
-      hide: 800
+      show: 50,
+      hide: 100
     }
+  })
+;
+
+$('#post_listing')
+  .dropdown({
+    on: 'hover'
+  })
+;
+
+$('#user')
+  .dropdown({
+  	on: 'hover'
   })
 ;
